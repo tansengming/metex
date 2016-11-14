c3f1e144077506a737c03a09f7bc09da841dcd5e
SengMing Tan <tansengming@gmail.com>
2016-11-14 16:06:25 -0500

    initial commit

---
+++ lib/worker.ex
@@ -0,0 +1,48 @@
+defmodule Metex.Worker do
+  def loop do
+    receive do
+      {pid, city} ->
+        send(pid, {:ok, temperature_of(city)})
+        loop
+      _ ->
+        IO.puts "I don't know what to do!"
+    end
+  end
+
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}C"
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey}"
+  end
+
+  defp apikey do
+    "95549b465e440bfc57fa9b50c6815e3a"
+  end
+end
---
+++ test/metex_worker_test.exs
@@ -0,0 +1,7 @@
+defmodule Metex.WorkerTest do
+  use ExUnit.Case, async: true
+
+  # test ".url_for" do
+  #   assert Metex.Worker.url_for("Kuala Lumpur") != "Kuala Lumpur"
+  # end
+end
